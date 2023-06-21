fe2a9d731dfe4a588ecf7daf4e01cc3f99f7057f
ardaarslan47 <a.ardaarslan@hotmail.com>
2023-06-22 02:03:58 +0300

    miniProject done

--- miniProject/dist/index.js
+++ miniProject/dist/index.js
@@ -1,2 +1,39 @@
 "use strict";
-console.log('HEEEY');
+const todoInput = document.getElementById("todoInput");
+const form = document.querySelector("#todoForm");
+const list = document.querySelector("#todoList");
+function readTodos() {
+    const todosJson = localStorage.getItem("todos");
+    return todosJson === null ? [] : JSON.parse(todosJson);
+}
+const todos = readTodos();
+function saveTodod() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+window.addEventListener("DOMContentLoaded", () => {
+    todos.forEach(createTodo);
+});
+function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+        text: todoInput.value,
+        isCompleted: false,
+    };
+    todos.push(newTodo);
+    createTodo(newTodo);
+    saveTodod();
+    todoInput.value = "";
+}
+function createTodo(todo) {
+    const newLi = document.createElement("li");
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.checked = todo.isCompleted;
+    checkBox.addEventListener("change", function () {
+        todo.isCompleted = checkBox.checked;
+        saveTodod();
+    });
+    newLi.append(todo.text, checkBox);
+    list.append(newLi);
+}
+form.addEventListener("submit", handleSubmit);
--- miniProject/src/index.ts
+++ miniProject/src/index.ts
@@ -1,1 +1,50 @@
-console.log('HEEEY');
+const todoInput = document.getElementById("todoInput") as HTMLInputElement;
+const form = document.querySelector("#todoForm") as HTMLFormElement;
+const list = document.querySelector("#todoList") as HTMLUListElement;
+
+interface Todo {
+  text: string;
+  isCompleted: boolean;
+}
+
+function readTodos(): Todo[] {
+  const todosJson = localStorage.getItem("todos");
+  return todosJson === null ? [] : JSON.parse(todosJson);
+}
+const todos: Todo[] = readTodos();
+
+function saveTodod() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  todos.forEach(createTodo);
+});
+
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  const newTodo: Todo = {
+    text: todoInput.value,
+    isCompleted: false,
+  };
+  todos.push(newTodo);
+  createTodo(newTodo);
+  saveTodod();
+  todoInput.value = "";
+}
+
+function createTodo(todo: Todo) {
+  const newLi = document.createElement("li");
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  checkBox.checked = todo.isCompleted;
+  checkBox.addEventListener("change", function () {
+    todo.isCompleted = checkBox.checked;
+    saveTodod();
+  });
+
+  newLi.append(todo.text, checkBox);
+  list.append(newLi);
+}
+
+form.addEventListener("submit", handleSubmit);
